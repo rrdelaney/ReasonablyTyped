94f4752a0a5ad07c38aa68cc79fef954a2379d1d
Ryan Delaney <rrdelaney@outlook.com>
2017-05-23 13:15:48 -0400

    Use readFile instead of readFileSync in tests

--- test/compile.js
+++ test/compile.js
@@ -1,6 +1,6 @@
 const test = require('ava')
 const path = require('path')
-const { readdirSync, readFileSync } = require('fs')
+const { readdirSync, readFile } = require('fs')
 const { compile, format } = require('../')
 const fixtures = path.join(__dirname, 'fixtures')
 const fixture = file => path.join(fixtures, file)
@@ -9,15 +9,25 @@ const testFiles = readdirSync(fixtures)
   .filter(file => file.endsWith('.js'))
   .map(file => ({
     [file]: {
-      js: readFileSync(fixture(file)).toString(),
-      re: readFileSync(fixture(file.replace('.js', '.re'))).toString()
+      js: new Promise((resolve, reject) => {
+        readFile(fixture(file), (err, data) => {
+          if (err) return reject(err)
+          resolve(data.toString())
+        })
+      }),
+      re: new Promise((resolve, reject) => {
+        readFile(fixture(file.replace('.js', '.re')), (err, data) => {
+          if (err) return reject(err)
+          resolve(data.toString())
+        })
+      })
     }
   }))
   .reduce((all, mod) => Object.assign({}, all, mod), {})
 
 
-const compareSources = (t, { js, re }) => {
-  t.is(compile(js), format(re))
+const compareSources = async (t, { js, re }) => {
+  t.is(compile(await js), format(await re))
 }
 
 Object.entries(testFiles).forEach(([moduleName, source]) => {
